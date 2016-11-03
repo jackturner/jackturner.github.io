e753003d43cad7a8dcc2d12bd6e4c41283afc945
Jack Turner <jack@turner.io>
2016-11-03 15:06:11 -0400

    I got straight lines going from the timeline markers/events (on the left) to the info on the events (on the right). Neat!

--- js/turner.io.js
+++ js/turner.io.js
@@ -1,23 +1,43 @@
 var s = Snap( "#cords" )
 
-var l = s.line( 0, 100, 200, 215 ).attr( { stroke: 'rgb(0,0,0)', 'stroke-width': 2 } )
-var l = s.line( 0, 120, 200, 310 ).attr( { stroke: 'rgb(0,0,0)', 'stroke-width': 2 } )
-var l = s.line( 0, 190, 200, 400 ).attr( { stroke: 'rgb(0,0,0)', 'stroke-width': 2 } )
+// var l = s.line( 0, 100, 200, 215 ).attr( { stroke: 'rgb(0,0,0)', 'stroke-width': 2 } )
+// var l = s.line( 0, 120, 200, 310 ).attr( { stroke: 'rgb(0,0,0)', 'stroke-width': 2 } )
+// var l = s.line( 0, 190, 200, 400 ).attr( { stroke: 'rgb(0,0,0)', 'stroke-width': 2 } )
 
 
 $(function() {
 
-	var $timeline = $("#timeline"),
+	var $body = $("body"),
+			$timeline = $("#timeline"),
+			$events = $("#events .event"),
 			born = moment("1981-08-01T15:00").unix(),
 			now = moment().unix(),
 			timespan = now - born
 
-	$("#events .event time").each(function() {
-		var date_str = $(this).attr("datetime")
-		var m = moment(date_str)
+	$events.each(function() {
+		var m = moment( $(this).find("time").attr("datetime") )
 		var percent =  (m.unix() - born) / timespan * 100
 		$("<div class='marker' />").css("bottom", percent + "%").appendTo($timeline)
-		console.log(top)
 	})
+
+	var $markers = $timeline.children(".marker")
+
+	var draw_lines = function() {
+
+		var timeline_top = $timeline.position().top
+
+		s.clear()
+
+		$markers.each(function(i, el) {
+			if(i==0) console.log($events.eq(i).scrollTop())
+			var marker_top = timeline_top + $(this).position().top + 10
+			var event_top = $events.eq(i).offset().top + 12 - $body.scrollTop()
+			var l = s.line( 0, marker_top, 200, event_top ).attr( { stroke: 'rgb(0,0,0)', 'stroke-width': 2 } )
+		})	
+
+	}
+
+	draw_lines()
+	$(window).scroll(draw_lines).resize(draw_lines)
 
 })
