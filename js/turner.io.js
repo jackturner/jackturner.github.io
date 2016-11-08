6fb3f96de5986876e7eb9ddec04b4665f3f6ee73
Jack Turner <jack@turner.io>
2016-11-07 21:30:29 -0500

    Starting to put in the correct time for events.

--- js/turner.io.js
+++ js/turner.io.js
@@ -25,16 +25,14 @@ $(function() {
 		var timeline_top = $timeline.position().top
 
 		$markers.each(function(i, el) {
-			var y1 = timeline_top + $(this).position().top + 10,
+			var y1 = timeline_top + $(this).position().top,
 					y2 = $events.eq(i).offset().top - $(window).scrollTop() + 12,
 					path_str = 'M0 ' + y1 + ' C ' + handle_distance + ' ' + y1 + ', ' + (cord_width - handle_distance) + ' ' + y2 + ', ' + cord_width + ' ' + y2
 
 			if ( has_drawn_cords )
 				$cords.eq(i).attr({"d": path_str})
 			else
-				// $cords[0].innerHTML += '<line x1="0" x2="190" y1="'+y1+'" y2="'+y2+'"></line>'
-			  $cords[0].innerHTML += '<path d="'+path_str+'" stroke="black" fill="transparent"/>'
-
+			  $cords[0].innerHTML += '<path d="'+path_str+'" stroke="#666" fill="transparent"/>'
 		})
 
 		if ( !has_drawn_cords ) {
